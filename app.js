ea608c3048751441e630ba952a862899943a35be
sajjadcodes <sakraan01@gmail.com>
2022-07-13 19:44:24 +0500

    updated version

--- app.js
+++ app.js
@@ -5,4 +5,14 @@ console.log(coinURL);
 $.getJSON(coinURL, function(data){
     console.log(data);
     console.log(data.disclaimer);
+    console.log(data.time.updated);
+    document.getElementById("currentTime").innerHTML = "updated date " + data.time.updated;
+});
+
+
+$url = "https://pro-api.coingecko.com/api/v3/coins/list";
+
+$.getJSON($url, function(response) {
+
+    console.log("response data", response);
 });
